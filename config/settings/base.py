be45d6491a82e28c4a49ee51c0e106c560cc9388
Savio Abuga <savioabuga@gmail.com>
2017-07-08 14:58:39 +0300

    moving to casa

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,6 +50,7 @@ THIRD_PARTY_APPS = [
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
     'phonenumber_field',
+    'datetimewidget',
 ]
 
 # Apps specific for this project go here.
@@ -176,7 +177,7 @@ TEMPLATES = [
 ]
 
 # See: http://django-crispy-forms.readthedocs.io/en/latest/install.html#template-packs
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 # STATIC FILE CONFIGURATION
 # ------------------------------------------------------------------------------
